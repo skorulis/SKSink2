0ab3e2fa94ebed4c9876b76418259f0531af9d36
Alexander Skorulis <skorulis@gmail.com>
2016-07-26 17:34:17 +1000

    Add image operations

---
+++ SKSink/Classes/UIKit/UIImage+SKAdditions.h
@@ -0,0 +1,9 @@
+//  Created by Alexander Skorulis on 26/07/2016.
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (SKAdditions)
+
+- (UIImage*)reduceToPixelSize:(CGSize)pixelSize;
+
+@end
---
+++ SKSink/Classes/UIKit/UIImage+SKAdditions.m
@@ -0,0 +1,21 @@
+//  Created by Alexander Skorulis on 26/07/2016.
+
+#import "UIImage+SKAdditions.h"
+
+@implementation UIImage (SKAdditions)
+
+- (UIImage*)reduceToPixelSize:(CGSize)pixelSize {
+    if(self.pixelSize.width > pixelSize.width || self.pixelSize.height > pixelSize.height) {
+        CGSize scaledSize = CGSizeMake(pixelSize.width / self.scale, pixelSize.height / self.scale);
+        UIGraphicsBeginImageContextWithOptions(scaledSize,true,self.scale);
+        [self drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
+        UIImage* image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        return image;
+    } else {
+        return self;
+    }
+}
+
+
+@end
